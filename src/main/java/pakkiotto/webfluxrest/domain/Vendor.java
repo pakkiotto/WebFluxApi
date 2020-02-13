d9bb32622c2f322213a476bf70c604e8c0372cbf
Carmine Parisi <carmine.parisi@assistdigital.com>
2020-02-13 16:31:07 +0100

    add all args constructor

--- src/main/java/pakkiotto/webfluxrest/domain/Vendor.java
+++ src/main/java/pakkiotto/webfluxrest/domain/Vendor.java
@@ -1,10 +1,14 @@
 package pakkiotto.webfluxrest.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@AllArgsConstructor
+@Builder
 @Document
 public class Vendor {
 

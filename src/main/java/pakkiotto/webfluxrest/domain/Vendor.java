e831c7eb2d872e4874e132eb9a4d0c2b34267aba
Carmine Parisi <carmine.parisi@assistdigital.com>
2020-02-13 15:21:21 +0100

    add domain

---
+++ src/main/java/pakkiotto/webfluxrest/domain/Vendor.java
@@ -0,0 +1,16 @@
+package pakkiotto.webfluxrest.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Vendor {
+
+    @Id
+    private String id;
+
+    private String lastname;
+    private String firstname;
+}

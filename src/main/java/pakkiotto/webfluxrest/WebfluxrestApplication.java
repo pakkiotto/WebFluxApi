12ff9916156a0547cdf87dde7dc48cb8edb99653
Carmine Parisi <carmine.parisi@assistdigital.com>
2020-02-13 12:38:54 +0100

    first app commit

---
+++ src/main/java/pakkiotto/webfluxrest/WebfluxrestApplication.java
@@ -0,0 +1,13 @@
+package pakkiotto.webfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxrestApplication.class, args);
+	}
+
+}
